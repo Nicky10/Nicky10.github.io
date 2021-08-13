8c4694fdd6c1c2fc6c62d64410206cb0ceb555c7
Nicolas Delgado <nfdelgado@indracompany.com>
2021-08-12 23:55:19 -0500

    Last Commit

---
+++ js/navmap.js
@@ -0,0 +1,24 @@
+function btnDurham() {
+  var popup = document.getElementById("btn-Durham");
+  popup.classList.toggle("show");
+}
+
+function btnCT() {
+  var popup = document.getElementById("btn-CT");
+  popup.classList.toggle("show");
+}
+
+function btnPeel() {
+  var popup = document.getElementById("btn-Peel");
+  popup.classList.toggle("show");
+}
+
+function btnHamilton() {
+  var popup = document.getElementById("btn-hamilton");
+  popup.classList.toggle("show");
+}
+
+function btnYork() {
+  var popup = document.getElementById("btn-York");
+  popup.classList.toggle("show");
+}
